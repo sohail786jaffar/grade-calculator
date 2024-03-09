d1efe507fb11e018327124f25c1536db2f976b70
sohail786jaffar <sohail.jaffar86@gmail.com>
2024-03-09 23:33:57 +0500

    grade calculator

---
+++ app.js
@@ -0,0 +1,29 @@
+let marks=prompt("enter your marks: ")
+if (marks>=90)
+{
+    alert("your grade is A++")
+}
+else if (marks>=80)
+{
+    alert("your grade is A+")
+}
+else if (marks>=70)
+{
+    alert("your grade is A")
+}
+else if (marks>=60)
+{
+    alert("your grade is B")
+}
+else if (marks>=50)
+{
+    alert("your grade is C")
+}
+else if (marks>=40)
+{
+    alert("your grade is D")
+}
+else{
+    alert("your grade is F")
+}
+location.reload()
