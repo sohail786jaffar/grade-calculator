7d95197d878cd6a200a1db9b4a83bec01ef4c88a
sohail786jaffar <sohail.jaffar86@gmail.com>
2024-03-11 01:42:58 +0500

    m

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-let marks=prompt("enter your marks: ")
-if (marks>=90)
+function grade (percentage){
+    if (percentage>=90)
 {
     alert("your grade is A++")
 }
-else if (marks>=80)
+else if (percentage>=80)
 {
     alert("your grade is A+")
 }
-else if (marks>=70)
+else if (percentage>=70)
 {
     alert("your grade is A")
 }
-else if (marks>=60)
+else if (percentage>=60)
 {
     alert("your grade is B")
 }
-else if (marks>=50)
+else if (percentage>=50)
 {
     alert("your grade is C")
 }
-else if (marks>=40)
+else if (percentage>=40)
 {
     alert("your grade is D")
 }
 else{
     alert("your grade is F")
 }
-location.reload()
+}
+let English=parseInt(prompt("enter your marks in English: ") )                   
+let Math=parseInt(prompt("enter your marks in Math: "))
+let Urdu=parseInt(prompt("enter your marks in Urdu: "))
+let Computer=parseInt(prompt("enter your marks in Computer: "))
+let physics=parseInt(prompt("enter your marks in Physics: "))
+let obtain_marks=English+Computer+Urdu+physics+Math
+alert("you total obtain marks is: " + obtain_marks)
+let percentage=(obtain_marks/500)*100
+alert(grade(percentage))
